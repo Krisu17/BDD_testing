09243091300a501b87cd63006a34c458e55f0d8d
pawel <spawel114@gmail.com>
2021-01-09 21:26:21 +0100

    Poprawka email.feature

--- email_validator.py
+++ email_validator.py
@@ -3,15 +3,15 @@ import re
 def email_validator(email):
 
     if '@' not in email:
-        return 1
+        return "1"
 
     if email.startswith('@'):
-        return 2
+        return "2"
 
     if email.endswith('@'):
-        return 3
+        return "3"
     
     if re.match(r"[^@]+@[^@]+\.[^@]+", email):
-        return 0
+        return "0"
     else:
-        return 4
+        return "4"
