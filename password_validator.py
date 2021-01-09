3800cbe2e9efabf6f12d2bb96e47518907a15426
pawel <spawel114@gmail.com>
2021-01-09 21:41:35 +0100

    Modyfikacja return w password_validator.

--- password_validator.py
+++ password_validator.py
@@ -7,35 +7,35 @@ def validate(password):
     lower = string.ascii_lowercase
     special_symols = '!@#$%^&*()-+={[}]\:;<,>.?/'
     if 8 > len(password):
-        return 1
+        return "1"
     status = 0
     for c in password:
         if c in upper:
             status = status + 1
             break
     if status == 0:
-        return 2
+        return "2"
     status = 0
     for c in password:
         if c in lower:
             status = status + 1
             break
     if status == 0:
-        return 3
+        return "3"
     status = 0
     for c in password:
         if c in digits:
             status = status + 1
             break
     if status == 0:
-        return 4
+        return "4"
     status = 0
     for c in password:
         if c in special_symols:
             status = status + 1
             break
     if status == 0:
-        return 5
-    return 0
+        return "5"
+    return "0"
 
 
