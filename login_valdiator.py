87b7201e9bd237b66bb6b74ef4c43ab3de30c9ee
redb3rry <michalmalinowski99@yahoo.co.uk>
2021-01-09 21:38:43 +0100

    Update login_valdiator.py

--- login_valdiator.py
+++ login_valdiator.py
@@ -4,11 +4,11 @@ import re
 def checkLogin(login):
     login_length = len(login)
     if login_length < 5:
-        return 1
+        return "1"
     checked_login = re.search("^[a-zA-Z]+$", login)
     if checked_login is None:
-        return 2
-    return 0
+        return "2"
+    return "0"
 
 
 if __name__ == '__main__':
