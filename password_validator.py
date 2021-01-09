60d3bdf929515c03c288b849e9340bef765a6a6b
kczaj <kubapcz@gmail.com>
2021-01-09 19:58:05 +0100

    Dodanie walidatora hasla

---
+++ password_validator.py
@@ -0,0 +1,41 @@
+import string
+
+
+def validate(password):
+    digits = string.digits
+    upper = string.ascii_uppercase
+    lower = string.ascii_lowercase
+    special_symols = '!@#$%^&*()-+={[}]\:;<,>.?/'
+    if len(password) < 8:
+        return 1
+    status = 0
+    for c in password:
+        if c in upper:
+            status = status + 1
+            break
+    if status == 0:
+        return 2
+    status = 0
+    for c in password:
+        if c in lower:
+            status = status + 1
+            break
+    if status == 0:
+        return 3
+    status = 0
+    for c in password:
+        if c in digits:
+            status = status + 1
+            break
+    if status == 0:
+        return 4
+    status = 0
+    for c in password:
+        if c in special_symols:
+            status = status + 1
+            break
+    if status == 0:
+        return 5
+    return 6
+
+
