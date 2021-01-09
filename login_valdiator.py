30e67ec76b08f0df19406f01d671c223487e8dc7
redb3rry <michalmalinowski99@yahoo.co.uk>
2021-01-09 20:10:02 +0100

    Create login_valdiator.py

---
+++ login_valdiator.py
@@ -0,0 +1,16 @@
+import re
+
+
+def checkLogin(login):
+    login_length = len(login)
+    if login_length < 5:
+        return 1
+    checked_login = re.search("^[a-zA-Z]+$", login)
+    if checked_login is None:
+        return 2
+    return 0
+
+
+if __name__ == '__main__':
+    login = "testlogin"
+    print(checkLogin(login))
